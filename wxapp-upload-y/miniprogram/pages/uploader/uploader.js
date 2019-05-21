f7a4fb0fd38478bd515630129933a43ca377cbf3
laoliyu <18170877760@qq.com>
2019-05-21 17:37:51 +0800

    521

---
+++ wxapp-upload-y/miniprogram/pages/uploader/uploader.js
@@ -0,0 +1,49 @@
+const app = getApp();
+Page({
+
+  data: {
+    files: []
+  },
+  chooseImage(e) {
+    let that = this
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],//原图和压缩图也行
+      sourceType: ['album', 'camera'],//拍照或者相册
+      success(res) {
+        console.log(res)
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        })
+        // 
+        for (let i = 0; i < res.tempFilePaths.length; i++) {
+
+          const filePath = res.tempFilePaths[i]
+          let a = filePath.lastIndexOf('.')
+          let b = filePath.lastIndexOf('.', filePath - 7)
+          let c = filePath.substring(b + 1, a)
+          const cloudPath = c + filePath.match(/\.[^.]+?$/)
+          wx.cloud.uploadFile({
+            cloudPath,
+            filePath,
+            success: (res) => {
+              // console.log(res.fileID)
+              console.log('上传成功', res)
+            },
+            fail: err => {
+
+            }
+          })
+
+        } 
+      }
+    })
+  },
+  previewImage(e) {
+    console.log(e)
+    wx.previewImage({
+      current: e.currentTarget.id,
+      urls: this.data.files//当前需要预览的图片效果
+    })
+  }
+})
+

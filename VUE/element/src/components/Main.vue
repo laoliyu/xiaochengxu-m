e4d210bf17c4e2812043b3e9475f95a17b05fcf4
laoliyu <18170877760@qq.com>
2019-05-30 17:46:44 +0800

    530

---
+++ VUE/element/src/components/Main.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-content">
+    <Header></Header>
+    <Slide></Slide>
+    <!-- 二级路由 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/common/Header";
+import Slide from "@/components/common/Slide";
+export default {
+  name: "mianContent",
+  data() {
+    return {
+      msg: "hello world"
+    };
+  },
+  components: {
+    Header,
+    Slide
+  }
+};
+</script>

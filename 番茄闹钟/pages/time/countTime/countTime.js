059abab8b0af595d6eb730004cae7572fb11f365
laoliyu <18170877760@qq.com>
2019-06-09 15:16:39 +0800

    2

--- 番茄闹钟/pages/time/countTime/countTime.js
+++ 番茄闹钟/pages/time/countTime/countTime.js
@@ -1,4 +1,4 @@
-// pages/time/time.js
+// pages/time/countTime/countTime.js
 Page({
 
   /**
@@ -7,18 +7,14 @@ Page({
   data: {
 
   },
-  
+
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
-  countTime: function () {
-    wx.navigateTo({
-      url: '../time/countTime/countTime'
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */

d8b1180d33f632b51a3d0107a03f311d40f857a9
laoliyu <18170877760@qq.com>
2019-06-18 20:02:53 +0800

    1

---
+++ VUE/full-start/element-starter/src/assets/home.vue
@@ -0,0 +1,16 @@
+<template></template>
+
+<script>
+export default {
+  mounted() {
+    fetch("http://localhost:8010/api/user/login")
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+      });
+  }
+};
+</script>
+
+<style>
+</style>

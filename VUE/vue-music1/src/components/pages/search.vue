f70aa1f07c22f3b11775cfe36fa9e609ed0f81d5
laoliyu <18170877760@qq.com>
2019-06-24 12:04:24 +0800

    1

--- VUE/vue-music1/src/components/pages/search.vue
+++ VUE/vue-music1/src/components/pages/search.vue
@@ -2,21 +2,49 @@
   <div class="search">
     <div class="search-box-wrapper">
       <!-- 搜索框 -->
-      <router-view></router-view> 
-      <!-- 引入路由 -->
       <searchBox></searchBox>
+    </div>
+    <div class="shortcut-wrapp" ref="shortcutWrapper" v-show="!query">
+      <!-- 热门搜索 -->
+      <scroll class="shortcut" ref="shortcut" :data="shortcut" :refreshDelay="refreshDelay">
+        <div>
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li
+              class="item"
+              v-for="(item,index) in hotKey"
+              :key="index"
+              @click="addQuery(item.first)"
+            >
+            <span>{{item.first}}</span></li>
+          </ul>
+        </div>
+      </scroll>
     </div>
   </div>
 </template>
 
 <script>
 import searchBox from "../searchBox";
+import scroll from "../scroll";
 export default {
   data() {
-    return {};
+    return {
+      query: false,
+      shortcut:[],
+      refreshDelay:0,
+      hotKey: [
+        { first: "shenghaohaonan" },
+        { first: "shenghaohaonan!" },
+        { first: "shenghaohaonan!!!" },
+        { first: "shenghaohaonan!!" }
+      ]
+    };
   },
+ 
   components: {
-    searchBox
+    searchBox,
+    scroll
   }
 };
 </script>

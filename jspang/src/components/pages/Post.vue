0fb95c0b543aa8ef4bf292d21cde0254ac3d6b8b
laoliyu <18170877760@qq.com>
2019-06-30 16:48:02 +0800

    1

--- jspang/src/components/pages/Post.vue
+++ jspang/src/components/pages/Post.vue
@@ -1,30 +1,44 @@
 <template>
   <div class="pos">
-      <el-row>
-          <el-col :span="7" class="post-order" id="order-list">我是点单栏</el-col>
-          <el-col>我是产品栏</el-col>
-
-      </el-row>
+    <el-row>
+      <el-col :span="7" class="post-order" id="order-list">
+        <el-tabs>
+          <el-tab-pane label="点餐">
+            <el-table border style="width:100%">
+              <el-table-column prop="goodsName" label="商品名称"></el-table-column>
+              <el-table-column prop="count" label="数量"></el-table-column>
+              <el-table-column prop="price" label="金额"></el-table-column>
+              <el-table-column label="操作" fixed="right">
+                <template scope="scope">
+                    <el-button type="text" size="small"></el-button>
+                </template>
+              </el-table-column>
+            </el-table>
+          </el-tab-pane>
+          <el-tab-pane label="挂单">挂单</el-tab-pane>
+          <el-tab-pane label="外卖">外卖</el-tab-pane>
+        </el-tabs>
+      </el-col>
+      <el-col>我是产品栏</el-col>
+    </el-row>
   </div>
-
 </template>
 
 <script>
 export default {
   name: "pos",
-  mounted:function(){
-      var orderHeight = document.body.clientHeight;
-      console.log(orderHeight)
-      document.getElementById('order-list').style.height=orderHeight+'px'
-
+  mounted: function() {
+    var orderHeight = document.body.clientHeight;
+    console.log(orderHeight);
+    document.getElementById("order-list").style.height = orderHeight + "px";
   }
 };
 </script>
 
 <style>
-.post-order{
-    background-color: #F9FAFC;
-    border-right: 1px solid #C0ccda;
-
+.post-order {
+  background-color: #f9fafc;
+  border-right: 1px solid #c0ccda;
+  height: 100%;
 }
 </style>
